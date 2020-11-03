582d18af48da9c3fd9ad350365e9c3798df9dab7
Roy Ivy III <rivy.dev@gmail.com>
2020-11-03 14:19:17 -0600

    maint/dev ~ change from 'standard' to ESLint/Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+// spell-checker:ignore (names) rivy
+module.exports = {
+	root: true,
+	env: { es6: true },
+	ignorePatterns: ['.eslintrc.js', '.nyc_output', 'build', 'coverage', 'dist', 'node_modules'],
+	// parser: '@typescript-eslint/parser',
+	// avoid `parserOptions` ~ [2020-10-29]/rivy ~ use is causing issues for eslint evaluation of files outside of `src` (see https://github.com>    // parserOptions: { project: ['./tsconfig.json', './tsconfig.eslint.json'] },
+	// plugins: ['import' , 'functional', '@typescript-eslint'],
+	plugins: ['import'],
+	extends: [
+		'eslint:recommended',
+		// 'plugin:@typescript-eslint/recommended',
+		'plugin:eslint-comments/recommended',
+		// 'plugin:import/typescript',
+		// 'plugin:functional/lite',
+		'prettier',
+		// 'prettier/@typescript-eslint',
+	],
+	rules: {
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
+		'eslint-comments/no-unused-disable': 'error',
+		'import/order': ['error', { 'newlines-between': 'always', alphabetize: { order: 'asc' } }],
+		'sort-imports': ['error', { ignoreDeclarationSort: true, ignoreCase: true }],
+	},
+	overrides: [
+		{
+			files: ['*.js'],
+			rules: {
+				'@typescript-eslint/no-var-requires': 'off',
+			},
+		},
+	],
+};
